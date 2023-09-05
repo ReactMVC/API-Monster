05fd56ce9e1bccce7a8406b6e0b535b234ce235d
Hossein Pira <120723120+ReactMVC@users.noreply.github.com>
2023-09-05 16:17:06 +0330

    1.6.2

---
+++ views/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API Monster</title>
+</head>
+
+<body>
+    <h1>Hello <?= $name ?></h1>
+</body>
+
+</html>
