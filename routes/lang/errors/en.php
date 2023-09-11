ef441aa5e20d6fb99d2fa51b2ad07624e6360f22
Hossein Pira <120723120+ReactMVC@users.noreply.github.com>
2023-09-11 11:46:08 +0330

    Add Data Validation Errors

---
+++ routes/lang/errors/en.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'required' => 'The :key field is required.',
+    'min' => 'The :key must be at least :value characters.',
+    'max' => 'The :key may not be greater than :value characters.',
+    'email' => 'The :key must be a valid email address.',
+    'regex' => 'The :key format is invalid.',
+    'number' => 'The :key field must be a number.',
+    'url' => 'The :key must be a valid URL.',
+    'error' => 'Unknown error for :key',
+];
